67403fda1fb32db377db5cc0a2188e2c9e6e79c4
Shafi <shadikulshafi@yahoo.com>
2023-05-20 23:39:47 +0600

    project created

---
+++ lib/view/screens/home.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HomeScreenPage extends StatelessWidget {
+  const HomeScreenPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
